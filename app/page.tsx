cf54cdf6a85e85e02310c93865f10e1b64da4010
Deus lo Vult <moises.barrera.dev@gmail.com>
2024-11-08 04:15:15 -0300

    se agrega toda la logica de busqueda en la bd

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,23 @@ export default function Home() {
 	}, [input]);
 
 	return (
-		<div>
-			<input
-				value={input}
-				onChange={(e) => setInput(e.target.value)}
-				type='text'
-			/>
-		</div>
+		<main className='h-screen w-screen grainy'>
+			<div className='flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5'>
+				<h1 className='text-5xl tracking-tight font-bold '>SpeedSearch</h1>
+				<p className='text-zinc-600 text-lg max-w-prose text-center'>
+					Experience the power of lightning-fast search with our cutting-edge API, built on a robust foundation of Hono,
+					Nextjs, and Cloudflare.
+					<br />
+					Enter your query below and discover the speed and precision that sets us apart from the rest
+				</p>
+				<div className='max-w-md w-full'></div>
+				<input
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					className='text-zinc-900 border border-zinc-300 max-w-sm rounded-lg px-4 py-2 w-full'
+					type='text'
+				/>
+			</div>
+		</main>
 	);
 }
